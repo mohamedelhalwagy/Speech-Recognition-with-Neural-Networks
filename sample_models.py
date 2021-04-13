97347f8dc1e4fa9c16db34f441186d602569ead4
Mohamed Abdelkader ElHalwagy <36703563+Theabdelkader@users.noreply.github.com>
2021-04-13 20:09:34 +0200

    CNN_RNN_MODEL

--- sample_models.py
+++ sample_models.py
@@ -29,9 +29,11 @@ def rnn_model(input_dim, units, activation, output_dim=29):
     simp_rnn = GRU(units, activation=activation,
         return_sequences=True, implementation=2, name='rnn')(input_data)
     # TODO: Add batch normalization 
-    bn_rnn = BatchNormalization()(simp_rnn)
+    bn_rnn = BatchNormalization() (simp_rnn)
     # TODO: Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = TimeDistributed(Dense(output_dim))(bn_rnn)
+    # TimeDistributedDense layer(wraper) :
+    # This wrapper allows us to apply a layer to every temporal slice of an input:
+    time_dense = TimeDistributed (Dense (output_dim) ) (bn_rnn)
     # Add softmax activation layer
     # Softmax converts a real vector to a vector of categorical probabilities.
     y_pred = Activation('softmax', name='softmax')(time_dense)
@@ -60,9 +62,9 @@ def cnn_rnn_model(input_dim, filters, kernel_size, conv_stride,
     simp_rnn = SimpleRNN(units, activation='relu',
         return_sequences=True, implementation=2, name='rnn')(bn_cnn)
     # TODO: Add batch normalization
-    bn_rnn = ...
+    bn_rnn = BatchNormalization(name="bn_rnn")(simp_rnn)
     # TODO: Add a TimeDistributed(Dense(output_dim)) layer
-    time_dense = ...
+    time_dense = TimeDistributed(Dense(output_dim))(bn_rnn)
     # Add softmax activation layer
     y_pred = Activation('softmax', name='softmax')(time_dense)
     # Specify the model
